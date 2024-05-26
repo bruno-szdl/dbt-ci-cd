4ffa0e7c3a84b1b6a2fe984444e9340f53f33ac7
Bruno Souza de Lima <bsouzadelima@phdata.io>
2024-05-26 20:50:29 -0300

    changin drop command from dbt_project to run operation

---
+++ macros/drop_schema.sql
@@ -0,0 +1,11 @@
+{% macro drop_schema() %}
+
+    {% set query %}
+        drop schema "{{ target.schema }}"
+    {% endset %}
+
+    {% if target.name == 'pr' %}
+        {{ run_query (query) }}
+    {% endif %}
+
+{% endmacro %}
